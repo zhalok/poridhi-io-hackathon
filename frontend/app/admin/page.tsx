2dde5e2a1fa8b43da9baebf9c27fb9a3cb08a0b0
Nafi Ullah Shafin <shafinnafiullah@gmail.com>
2025-04-26 10:21:17 +0600

    added admin upload page

---
+++ frontend/app/admin/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { FileUploadDemo } from './FileUpload'
+
+const Admin = () => {
+  return (
+    <div className='flex flex-col items-center justify-center min-h-screen bg-[#1E1E1E]'>
+        <FileUploadDemo/>
+    </div>
+  )
+}
+
+export default Admin
